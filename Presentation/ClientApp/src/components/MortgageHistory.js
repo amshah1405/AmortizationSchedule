62f66d0708a55232f905232426ca8ab2f28e15f7
amshah1405 <amshah1405@gmail.com>
2023-09-18 04:33:48 -0400

    Added a component to show mortgage history

---
+++ Presentation/ClientApp/src/components/MortgageHistory.js
@@ -0,0 +1,52 @@
+﻿import React, { useState, useEffect } from 'react';
+
+const AccordionItem = ({ title, content, isOpen, toggleAccordion }) => (
+    <div className={`accordion-item ${isOpen ? 'open' : ''}`}>
+        <div className="accordion-header" onClick={toggleAccordion}>
+            {title}
+            <span className={`icon ${isOpen ? 'open' : ''}`}>▼</span>
+        </div>
+        {isOpen && <div className="accordion-content">{content}</div>}
+    </div>
+);
+
+ 
+
+    const MortgageHistory = () => {
+        const [mortgageHistory, setMortgageHistory] = useState([]);
+        const [activeIndex, setActiveIndex] = useState(null);
+
+        useEffect(() => {
+            fetch('https://localhost:7182/api/Mortgage/retrieveHistory')
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    setMortgageHistory(data);
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }, []);
+
+        const toggleAccordion = (index) => () => {
+            setActiveIndex(index === activeIndex ? null : index);
+        };
+        return (
+            <div className="accordion">
+                {mortgageHistory.map((item, index) => (
+                    <AccordionItem
+                        key={index}
+                        title={item.title}
+                        content={item.content}
+                        isOpen={index === activeIndex}
+                        toggleAccordion={toggleAccordion(index)}
+                    />
+                ))}
+            </div>
+        );
+    };
+export default MortgageHistory;
